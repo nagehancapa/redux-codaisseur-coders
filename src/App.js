6296d8fa7875f11d54f0827ec200ae0bb394f109
nagehancapa <capanagehan@gmail.com>
2021-05-29 11:23:13 +0200

    added authentication

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/login" component={LoginPage} />
         <Route path="/post/:id" component={PostPage} />
         <Route exact path="/" component={HomePage} />
       </Switch>
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../store/auth/actions";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log("smt", email, password);
+    dispatch(login(email, password));
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Email:{" "}
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Password:{" "}
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Login</button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
